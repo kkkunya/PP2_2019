b48820c869f5fd156c6f2226631e4ebdbcc58e0a
kkkunya <kush01@mail.ru>
2019-02-15 12:44:56 +0600

    third commit

--- Week2/Task4/Program.cs
+++ Week2/Task4/Program.cs
@@ -16,7 +16,7 @@ namespace Task4
                 file.WriteLine("Smile:) have a great day!");//writing something to inside of the file
                 file.Close();//closing the file
                              //we have another method of creating the file and writing something inside though below given line
-                             //"C:\Users\Aida\Desktop\pp2\PP2\week2\Task4\path\file.txt","Smile:) have a great day!")
+                             //"C:\Users\Lenovo\Desktop\PP2_2019\Week2\Task4\path\file.txt","Smile:) have a great day!")
 
                 File.Copy("C:\Users\Lenovo\Desktop\PP2_2019\Week2\Task4/path/file.txt", "C:\Users\Lenovo\Desktop\PP2_2019\Week2\Task4\path1\file.txt");//copying the file to another directory
                 File.Delete("C:\Users\Lenovo\Desktop\PP2_2019\Week2\Task4\path\file.txt");//deleting the file
@@ -24,8 +24,8 @@ namespace Task4
 
             /*public static void CreateMoveFolder() // this function creates folder and moves it to another directory
            {
-               Directory.CreateDirectory("C:/Users/Aida/Desktop/pp2/PP2/week2/Task4/path/file");
-               Directory.Move("C:/Users/Aida/Desktop/pp2/PP2/week2/Task4/path/file", "C:/Users/Aida/Desktop/pp2/PP2/week2/Task4/path1/file");
+               Directory.CreateDirectory("C:\Users\Lenovo\Desktop\PP2_2019\Week2\Task4/path/file.txt");
+               Directory.Move("C:\Users\Lenovo\Desktop\PP2_2019\Week2\Task4\path1\file.txt", "C:\Users\Lenovo\Desktop\PP2_2019\Week2\Task4\path1\file.txt");
            }*/
 
             static void Main(string[] args)
