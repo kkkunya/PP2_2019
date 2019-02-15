cdd8b4577de9a7a84c875eb3973282c373cd408d
kkkunya <kush01@mail.ru>
2019-02-15 12:42:08 +0600

    second commit

--- Week2/Task4/Program.cs
+++ Week2/Task4/Program.cs
@@ -12,14 +12,14 @@ namespace Task4
         {
             public static void CreateCopyDeleteFile()//funtion does operations like creating file and  copying file and then deleting file 
             {
-                StreamWriter file = new StreamWriter(@"C:\Users\Aida\Desktop\pp2\PP2\week2\Task4\path\file.txt");//creating file   
+                StreamWriter file = new StreamWriter(@"C:\Users\Lenovo\Desktop\PP2_2019\Week2\Task4\path\file.txt");//creating file   
                 file.WriteLine("Smile:) have a great day!");//writing something to inside of the file
                 file.Close();//closing the file
                              //we have another method of creating the file and writing something inside though below given line
                              //"C:\Users\Aida\Desktop\pp2\PP2\week2\Task4\path\file.txt","Smile:) have a great day!")
 
-                File.Copy("C:/Users/Aida/Desktop/pp2/PP2/week2/Task4/path/file.txt", "C:/Users/Aida/Desktop/pp2/PP2/week2/Task4/path1/file.txt");//copying the file to another directory
-                File.Delete("C:/Users/Aida/Desktop/pp2/PP2/week2/Task4/path/file.txt");//deleting the file
+                File.Copy("C:\Users\Lenovo\Desktop\PP2_2019\Week2\Task4/path/file.txt", "C:\Users\Lenovo\Desktop\PP2_2019\Week2\Task4\path1\file.txt");//copying the file to another directory
+                File.Delete("C:\Users\Lenovo\Desktop\PP2_2019\Week2\Task4\path\file.txt");//deleting the file
             }
 
             /*public static void CreateMoveFolder() // this function creates folder and moves it to another directory
